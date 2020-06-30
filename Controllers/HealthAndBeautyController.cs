51d79e83573378bb515d9ec8d6f8f2e4ca6f4491
dambou12 <39448477+dambou12@users.noreply.github.com>
2020-06-29 22:27:19 -0700

    Add files via upload

---
+++ Controllers/HealthAndBeautyController.cs
@@ -0,0 +1,91 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SmartBookingApp.Controllers
+{
+    
+    public class HealthAndBeautyController : Controller
+    {
+        private Manager m = new Manager();
+        // GET: HealthAndBeauty
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        // GET: HealthAndBeauty/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: HealthAndBeauty/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: HealthAndBeauty/Create
+        [HttpPost]
+        public ActionResult Create(FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add insert logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HealthAndBeauty/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: HealthAndBeauty/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add update logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: HealthAndBeauty/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: HealthAndBeauty/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, FormCollection collection)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+
+                return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ Models/ViewModels/RestaurantsBaseViewModel.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+
+namespace SmartBookingApp.Models.ViewModels
+{
+    public class RestaurantsBaseViewModel
+    {
+
+        //Restaurant, Breakfast, Bistro and Bakeries
+        [Key]
+        public int Id { get; set; }
+
+        [Display(Name = "Coordinator who looks after the Restaurants")]
+        public string Coordinator { get; set; }
+
+        [Required, StringLength(50)]
+        [Display(Name = "Restaurant Name")]
+        public string Name { get; set; }
+
+        [Required, StringLength(50)]
+        [Display(Name = "Adress")]
+        public string location { get; set; }
+
+        [Display(Name = "Restaurant image (cover art)")]
+        public string UrlAlbum { get; set; }
+
+    }
+}
